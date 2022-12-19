abf8e4b6724a1697893a75a5a74fa0be8c3817ea
Gabriel Luz <Gabriel_Luz@mckinsey.com>
2022-12-19 13:22:21 -0300

    add transition data class object

---
+++ ddpg/replay_buffer/transition.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass
+class Transition:
+    """Class to store information regarding RL transitions."""
+
+    state: np.ndarray
+    action: np.ndarray
+    reward: float
+    next_state: np.ndarray
+    done: bool
