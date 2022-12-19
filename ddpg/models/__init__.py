599fffe078f8affbbdb0808fb8675ad57e2646b3
Gabriel Luz <Gabriel_Luz@mckinsey.com>
2022-12-19 16:02:57 -0300

    add functions to save and load actor/critic models

--- ddpg/models/__init__.py
+++ ddpg/models/__init__.py
@@ -1,2 +1,3 @@
 from ddpg.models.actor import Actor
 from ddpg.models.critic import Critic
+from ddpg.models.utils import save_model, load_model
---
+++ ddpg/models/utils.py
@@ -0,0 +1,39 @@
+from typing import Union
+import torch
+
+from ddpg.models.actor import Actor
+from ddpg.models.critic import Critic
+
+def save_model(model: Union[Actor, Critic], filepath: str) -> None:
+    """Function to save Actor/Critic models.
+
+    Args:
+        model (Union[Actor, Critic]): Actor/Critic model to be saved.
+        filepath (str): Name of the file where to save the model.
+    """
+    architecture = model.architecture()
+    architecture["state_dict"] = model.state_dict()
+    torch.save(architecture, filepath)
+
+def load_model(filepath: str, model_class: object) -> Union[Actor, Critic]:
+    """Function to load Actor/Critic models.
+
+    Args:
+        filepath (str): Name of the file where model is currently stored.
+        model_class (object): Class of object to be retrieved [Actor, Critic]
+
+    Returns:
+        Union[Actor, Critic]: Retrieved model
+    """
+    architecture = torch.load(filepath)
+    architecture["hidden_layers"] = list(map(int, architecture["hidden_layers"].split(" | ")))
+
+    state_dict = architecture["state_dict"]
+    del architecture["state_dict"]
+
+    model = model_class(**architecture)
+    model.load_state_dict(state_dict)
+
+    return model
+    
+
