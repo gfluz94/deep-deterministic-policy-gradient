db891ba29ad4c3da9243c591391709c229c08208
Gabriel Luz <Gabriel_Luz@mckinsey.com>
2022-12-19 14:17:52 -0300

    add docstrings due to linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+"""
+Module that contains objects used as inputs for test scenarios.
+"""
+
 from typing import Tuple
 import pytest
 import numpy as np
@@ -7,6 +11,11 @@ from ddpg.replay_buffer.transition import Transition
 
 @pytest.fixture(scope="module")
 def transition_not_done() -> Transition:
+    """Transition example for testing purposes, with `done` set to False.
+
+    Returns:
+        Transition: Mocked transition example for testing.
+    """
     return Transition(
         state=np.array([0.5, 0.25, 0.5]),
         action=np.array([1.0, -1.0, 1.0]),
@@ -18,6 +27,11 @@ def transition_not_done() -> Transition:
 
 @pytest.fixture(scope="module")
 def arrays_not_done() -> Tuple[np.ndarray]:
+    """Numpy arrays expected to be returned from sampling transitions.
+
+    Returns:
+        Tuple[np.ndarray]: Mocked transition example for testing.
+    """
     return (
         np.array([[0.5, 0.25, 0.5]]),
         np.array([[1.0, -1.0, 1.0]]),
@@ -29,6 +43,11 @@ def arrays_not_done() -> Tuple[np.ndarray]:
 
 @pytest.fixture(scope="module")
 def transition_done() -> Transition:
+    """Transition example for testing purposes, with `done` set to True.
+
+    Returns:
+        Transition: Mocked transition example for testing.
+    """
     return Transition(
         state=np.array([0.5, 0.25, 0.5]),
         action=np.array([1.0, -1.0, 1.0]),
@@ -40,6 +59,11 @@ def transition_done() -> Transition:
 
 @pytest.fixture(scope="module")
 def arrays_done() -> Tuple[np.ndarray]:
+    """Numpy arrays expected to be returned from sampling transitions.
+
+    Returns:
+        Tuple[np.ndarray]: Mocked transition example for testing.
+    """
     return (
         np.array([[0.5, 0.25, 0.5]]),
         np.array([[1.0, -1.0, 1.0]]),
